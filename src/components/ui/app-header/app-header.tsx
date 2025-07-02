33d134efab2dbd6fa7d5280f4019ad913f78e039
Anton Kurashkevich <mr.kur.anton@gmail.com>
2025-07-02 21:01:49 +0300

    fix: icons in header changing colors depending on route

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -7,7 +7,7 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
@@ -20,22 +20,35 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
             clsx(styles.link, isActive && styles.link_active)
           }
         >
-          <BurgerIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
+          {({ isActive }) => (
+            <>
+              <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2 mr-10'>
+                Конструктор
+              </p>
+            </>
+          )}
         </NavLink>
+
         <NavLink
           to='/feed'
           className={({ isActive }) =>
             clsx(styles.link, isActive && styles.link_active)
           }
         >
-          <ListIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>Лента заказов</p>
+          {({ isActive }) => (
+            <>
+              <ListIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>Лента заказов</p>
+            </>
+          )}
         </NavLink>
       </div>
+
       <div className={styles.logo}>
         <Logo className='' />
       </div>
+
       <div className={styles.link_position_last}>
         <NavLink
           to='/profile'
@@ -43,10 +56,14 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
             clsx(styles.link, isActive && styles.link_active)
           }
         >
-          <ProfileIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>
-            {userName || 'Личный кабинет'}
-          </p>
+          {({ isActive }) => (
+            <>
+              <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>
+                {userName || 'Личный кабинет'}
+              </p>
+            </>
+          )}
         </NavLink>
       </div>
     </nav>
